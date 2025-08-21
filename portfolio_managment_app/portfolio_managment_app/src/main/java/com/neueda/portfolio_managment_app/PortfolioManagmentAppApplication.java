e43b49d885f8f86716b523547857ff70452fa9c8
Justyna Gajdek <test@email.com>
2025-08-21 14:30:09 +0000

    init commit

---
+++ portfolio_managment_app/portfolio_managment_app/src/main/java/com/neueda/portfolio_managment_app/PortfolioManagmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.neueda.portfolio_managment_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioManagmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioManagmentAppApplication.class, args);
+	}
+
+}
---
+++ portfolio_managment_app/portfolio_managment_app/src/test/java/com/neueda/portfolio_managment_app/PortfolioManagmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neueda.portfolio_managment_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioManagmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
